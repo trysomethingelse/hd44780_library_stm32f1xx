5138fbc93ebaa87d9905f06a0d363dea29bc194b
trysomethingelse <ma2tig@gmail.com>
2018-09-17 19:14:15 +0200

    basic funcionality is working

---
+++ hd44780.c
@@ -0,0 +1,110 @@
+#include "hd44780.h"
+
+uint8_t busy_flag_check()
+{
+
+	uint8_t out = BUSY;
+
+	//must to set all data lines as input
+	GPIO_InitTypeDef GPIO_InitStruct;
+	GPIO_InitStruct.Pin = LCD_4567;
+	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+	HAL_GPIO_Init(LCD_DATA_PORT, &GPIO_InitStruct);
+	HAL_GPIO_WritePin(LCD_CONTROL_PORT,LCD_RW,GPIO_PIN_SET);//read from lcd
+	HAL_GPIO_WritePin(LCD_DATA_PORT,LCD_RS,GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(LCD_CONTROL_PORT,LCD_CLOCK,GPIO_PIN_SET);
+
+	for(int i = NO_BUSY_CHECK; i >  0; i-- )
+	{
+		if(HAL_GPIO_ReadPin(LCD_DATA_PORT,LCD_7) == NOT_BUSY)
+		{
+			out = NOT_BUSY;
+			break;
+		}
+		HAL_Delay(1);
+	}
+
+	//set data lines as output
+	GPIO_InitStruct.Pin = LCD_4567;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(LCD_DATA_PORT, &GPIO_InitStruct);
+	HAL_GPIO_WritePin(LCD_CONTROL_PORT,LCD_RW,GPIO_PIN_RESET);//write to lcd
+	//HAL_GPIO_WritePin(LCD_CONTROL_PORT,LCD_CLOCK,GPIO_PIN_RESET);
+
+
+
+	return out;
+}
+
+void lcd_init()
+{
+	lcd_write_nibble(SET_4BIT_OPERATION,MSB,0);
+	lcd_write(TWO_LINES_4BIT);
+	lcd_write(DISPLAY_ON_OFF);
+	lcd_write(ENTRY_MODE_INCREMENT | DISPLAY_SHIFT_OFF);
+}
+uint8_t get_bit(uint8_t command, uint8_t no)
+{
+	return ((command >> no) & 1);
+}
+void lcd_write_nibble(uint8_t command, uint8_t start_bit, uint8_t rs)
+{
+	uint32_t bsrr = 0;
+
+	if (get_bit(command,start_bit)) bsrr |= LCD_7; //set one
+	else bsrr |= ((uint32_t)LCD_7 <<16U);  //must to shift to left, because of BSRR from 16 place resets ODR
+
+	if (get_bit(command,start_bit-1)) bsrr |= LCD_6; //set one
+	else bsrr |= ((uint32_t)LCD_6 <<16U);  //must to shift to left, because of BSRR from 16 place resets ODR
+
+	if (get_bit(command,start_bit-2)) bsrr |= LCD_5; //set one
+	else bsrr |= ((uint32_t)LCD_5 <<16U);  //must to shift to left, because of BSRR from 16 place resets ODR
+
+	if (get_bit(command,start_bit-3)) bsrr |= LCD_4; //set one
+	else bsrr |= ((uint32_t)LCD_4 << 16U);  //must to shift to left, because of BSRR from 16 place resets ODR
+
+	LCD_DATA_PORT -> BSRR =  bsrr;
+
+
+	bsrr = 0;
+	bsrr = LCD_RW << 16U; //set zero on RW
+	if (rs) bsrr |= LCD_RS; //set one
+	else bsrr |= LCD_RS << 16U;  //must to shift to left, because of BSRR from 16 place resets ODR
+	LCD_CONTROL_PORT -> BSRR = bsrr;
+
+	lcd_clock();
+
+}
+uint8_t lcd_write(uint8_t command)
+{
+	if (busy_flag_check() == BUSY) return COMMAND_BUSY_ERROR;
+	lcd_write_nibble(command,MSB, 0);
+	lcd_write_nibble(command,LSB, 0);
+	return COMMAND_SUCCESS;
+}
+uint8_t lcd_write_letter(uint8_t letter)
+{
+
+	if (busy_flag_check() == BUSY) return COMMAND_BUSY_ERROR;
+	lcd_write_nibble(letter,MSB, 1);
+	lcd_write_nibble(letter,LSB, 1);
+	return COMMAND_SUCCESS;
+}
+int counter = 0;
+void lcd_clock()
+{
+	HAL_GPIO_WritePin(LCD_CONTROL_PORT, LCD_CLOCK,GPIO_PIN_SET);
+	HAL_Delay(1);
+	HAL_GPIO_WritePin(LCD_CONTROL_PORT, LCD_CLOCK,GPIO_PIN_RESET);
+	HAL_Delay(1);
+
+}
+uint8_t lcd_write_word(char word[])
+{
+	for(int i = 0; word[i] != 0; i++)
+	{
+		if(lcd_write_letter(word[i]) == COMMAND_BUSY_ERROR)return COMMAND_BUSY_ERROR;
+	}
+	return COMMAND_SUCCESS;
+}
---
+++ hd44780.h
@@ -0,0 +1,72 @@
+#ifndef HD44780_H
+
+#include "stm32f1xx_hal.h"
+
+//all lcd control ports must be in te same group eg. GPIOB
+#define LCD_CONTROL_PORT GPIOB
+
+#define LCD_RS GPIO_PIN_12
+#define LCD_RW GPIO_PIN_13
+#define LCD_CLOCK GPIO_PIN_14
+
+//all lcd data ports must be in te same group eg. GPIOA
+#define LCD_DATA_PORT GPIOA
+
+#define LCD_4 GPIO_PIN_8
+#define LCD_5 GPIO_PIN_9
+#define LCD_6 GPIO_PIN_10
+#define LCD_7 GPIO_PIN_11
+
+
+#define LCD_4567 LCD_4|LCD_5|LCD_6|LCD_7
+#define BUSY_FLAG LCD_7
+
+#define ON 1
+#define OFF 0
+#define BUSY 1
+#define NOT_BUSY 0
+#define NO_BUSY_CHECK 10
+
+#define COMMAND_BUSY_ERROR 1
+#define COMMAND_SUCCESS 0
+
+#define DISPLAY_CLEAR 0x01
+#define DISPLAY_HOME 0x02
+#define SET_4BIT_OPERATION 0x20
+#define TWO_LINES_4BIT 0x28
+#define DISPLAY_ON_OFF 0x0E
+
+#define DISPLAY_SHIFT_OFF 0x04
+#define DISPLAY_SHIFT_ON 0x05
+#define ENTRY_MODE_INCREMENT 0x06
+#define ENTRY_MODE_DECREMENT 0x04
+
+#define DISPLAY_SHIFT 0x18
+#define CURSOR_SHIFT 0x10
+#define SHIFT_LEFT 0x10
+#define SHIFT_RIGHT 0x14
+
+#define MSB 7
+#define LSB 3
+
+/**
+  * @brief Check busy flag
+  * @note
+
+
+  * @retval eror
+  */
+uint8_t busy_flag_check(void);
+
+void lcd_clock(void);
+void lcd_init(void);
+void lcd_write_nibble(uint8_t, uint8_t, uint8_t);
+uint8_t lcd_write(uint8_t);
+uint8_t lcd_write_letter(uint8_t);
+uint8_t get_bit(uint8_t, uint8_t);
+uint8_t lcd_write_word(char []);
+
+
+#endif  // HD44780_H
+
+
